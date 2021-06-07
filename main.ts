6ddd067ac57567c1159e1b46d4ddf17939fcb102
Jullian <83075638+JullianWasTaken@users.noreply.github.com>
2021-06-07 11:27:57 -0400

    Game updates

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 namespace SpriteKind {
     export const enemySpawn = SpriteKind.create()
     export const replacementBlock = SpriteKind.create()
+    export const neutral = SpriteKind.create()
+    export const explosionEndBlock = SpriteKind.create()
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (User.vx > 0) {
@@ -61,10 +63,21 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
             . . . . . . . . . . . . . . . . 
             `, User, 100, randint(5, 15))
     }
+    music.pewPew.play()
+    pause(shootingDelay)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (devTestChecker == true) {
+        spawnEnemies()
+    }
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.replacementBlock, function (sprite, otherSprite) {
+    sprite.destroy()
+    info.changeLifeBy(-1)
 })
 // Konami code :>
 controller.combos.attachCombo("uuddlrlrba", function () {
-    game.splash("The cake is a lie!")
+    developerTestMode()
 })
 function killStreakReward () {
     killStreak = 0
@@ -76,24 +89,120 @@ function intro () {
 }
 function lose () {
     game.over(false, effects.dissolve)
-    pause(2000)
 }
 function animationStart () {
 	
 }
-scene.onOverlapTile(SpriteKind.Enemy, assets.tile`myTile3`, function (sprite, location) {
-    lose()
-})
 info.onCountdownEnd(function () {
-    scene.cameraShake(5, 5000)
-    pause(2000)
-    game.over(true, effects.starField)
+    Robot.setKind(SpriteKind.neutral)
+    User.setKind(SpriteKind.neutral)
+    for (let value of tiles.getTilesByType(assets.tile`myTile4`)) {
+        boomBot = sprites.create(img`
+            c c c c c c c c c c c c c c c c 
+            c b b b b b b b b b b b b b b c 
+            c b c c b b b b b b b b 4 b b c 
+            c b c b b 4 4 b b b b b 4 b b c 
+            c b b b b 4 b b b 4 4 4 4 4 b c 
+            c b b 4 b b b b b 4 4 4 b 4 b c 
+            c b b 4 b b b b b 4 4 4 b 4 b c 
+            c b b b 4 4 b b b b 4 b b 4 b c 
+            c b b b 4 b b 4 b 4 4 b b b b c 
+            c b b 4 b b 4 4 b b 4 b b b b c 
+            c b b 4 4 4 4 b b b 4 b b b b c 
+            c b b b 4 b 4 4 b 4 4 4 b b b c 
+            c b b 4 4 4 4 b b 4 b 4 4 c b c 
+            c b b b b b b b b b b b c c b c 
+            c b b b b b b b b b b b b b b c 
+            c c c c c c c c c c c c c c c c 
+            `, SpriteKind.explosionEndBlock)
+        scene.cameraFollowSprite(boomBot)
+        tiles.placeOnTile(boomBot, value)
+        boomBot.destroy(effects.fire, 500)
+    }
+    scene.cameraShake(50, 7000)
+    music.bigCrash.play()
+    timer.after(1800, function () {
+        game.over(true, effects.starField)
+    })
 })
+function spawnEnemyMain () {
+    for (let index = 0; index <= 3; index++) {
+        Robot = sprites.create(img`
+            ........................
+            ........................
+            ........................
+            ........................
+            ..........ffff..........
+            ........ff1111ff........
+            .......fb111111bf.......
+            .......f11111111f.......
+            ......fd11111111df......
+            ......fd11111111df......
+            ......fddd1111dddf......
+            ......fbdbfddfbdbf......
+            ......fcdcf11fcdcf......
+            .......fb111111bf.......
+            ......fffcdb1bdffff.....
+            ....fc111cbfbfc111cf....
+            ....f1b1b1ffff1b1b1f....
+            ....fbfbffffffbfbfbf....
+            .........ffffff.........
+            ...........fff..........
+            ........................
+            ........................
+            ........................
+            ........................
+            `, SpriteKind.Enemy)
+        Robot.setPosition(randint(101, 137), randint(6, scene.screenWidth()))
+        distance = Math.sqrt((Robot.x - User.x) ** 2 + (Robot.y - User.y) ** 2)
+        while (distance < 31) {
+            Robot.setPosition(142, randint(6, 110))
+        }
+        Robot.follow(bomb, enemySpeed)
+        enemyAmount += 1
+    }
+}
+function spawnEnemies () {
+    for (let index = 0; index < 3; index++) {
+        Robot = sprites.create(img`
+            ........................
+            ........................
+            ........................
+            ........................
+            ..........ffff..........
+            ........ff1111ff........
+            .......fb111111bf.......
+            .......f11111111f.......
+            ......fd11111111df......
+            ......fd11111111df......
+            ......fddd1111dddf......
+            ......fbdbfddfbdbf......
+            ......fcdcf11fcdcf......
+            .......fb111111bf.......
+            ......fffcdb1bdffff.....
+            ....fc111cbfbfc111cf....
+            ....f1b1b1ffff1b1b1f....
+            ....fbfbffffffbfbfbf....
+            .........ffffff.........
+            ...........fff..........
+            ........................
+            ........................
+            ........................
+            ........................
+            `, SpriteKind.Enemy)
+        Robot.y = randint(5, 150)
+        Robot.x = 325
+        enemyAmount += 1
+        pause(200)
+    }
+}
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
-    info.changeLifeBy(-1)
-    sprite.destroy(effects.trail, 500)
-    music.powerDown.play()
+    sprite.setPosition(User.x + randint(10, 20), User.y + randint(10, 20))
     scene.cameraShake(4, 500)
+    info.changeLifeBy(-1)
+    music.powerDown.play()
+    invincibilityAfterHit()
+    User.say("Ow!")
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
     sprite.x = User.x + 6
@@ -105,6 +214,38 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, l
 info.onLifeZero(function () {
     lose()
 })
+function invincibilityAfterHit () {
+    for (let index = 0; index < 2; index++) {
+        User.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+        pause(50)
+        User.setImage(assets.image`Temporary asset0`)
+    }
+    pause(200)
+}
+function developerTestMode () {
+    info.stopCountdown()
+    User.say("Developer test mode enabled!")
+    info.setLife(5000)
+    User.setKind(SpriteKind.neutral)
+    shootingDelay = 0
+}
 // Delete enemy :D
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy(effects.fire, 100)
@@ -122,15 +263,21 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
     enemyAmount += -1
     scene.cameraShake(2, 100)
 })
-let Robot: Sprite = null
 let enemyAmount = 0
+let distance = 0
+let boomBot: Sprite = null
+let Robot: Sprite = null
 let projectile: Sprite = null
+let shootingDelay = 0
+let enemySpeed = 0
 let killStreakRequirement = 0
 let killStreak = 0
 let yAcceleration = 0
 let xAcceleration = 0
 let User: Sprite = null
 let bomb: Sprite = null
+let devTestChecker = false
+devTestChecker = false
 // used because the title thing takes too much space
 intro()
 tiles.setTilemap(tilemap`level1`)
@@ -193,16 +340,25 @@ User = sprites.create(img`
 xAcceleration = 100
 yAcceleration = 100
 scene.cameraFollowSprite(User)
-let maxEnemy = 10
+let maxEnemy = 15
 info.setLife(3)
 info.setScore(0)
 User.setPosition(143, 105)
-info.startCountdown(randint(45, 240))
+info.startCountdown(randint(5, 6))
 killStreak = 0
 killStreakRequirement = 5
-let enemySpeed = 20
+enemySpeed = 40
+shootingDelay = 200
 game.onUpdate(function () {
-    Robot.follow(bomb, enemySpeed)
+    if (devTestChecker == true) {
+        xAcceleration = 200
+        yAcceleration = 200
+    }
+})
+forever(function () {
+    if (User.vx >= 1 || User.vx <= -1 || (User.vy >= 1 || User.vy <= -1)) {
+        scene.cameraShake(2, 100)
+    }
 })
 forever(function () {
     if (User.vx < 0) {
@@ -215,29 +371,7 @@ forever(function () {
 })
 game.onUpdateInterval(randint(5000, 30000), function () {
     if (enemyAmount < maxEnemy) {
-        for (let index = 0; index < 3; index++) {
-            Robot = sprites.create(img`
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                2 2 2 2 2 . 2 2 2 2 . 2 2 2 . . 
-                2 . . . 2 . 2 . . 2 . 2 . . . . 
-                2 2 2 2 2 . 2 . . 2 . 2 2 2 . . 
-                2 . . . . . 2 . . 2 . 2 . . . . 
-                2 2 2 2 2 . 2 . . 2 . 2 2 2 2 . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . 2 2 . . 2 2 . . 2 . . 2 . . . 
-                . 2 . 2 2 . 2 . . 2 2 2 2 . . . 
-                . 2 . . . . 2 . . . . . 2 . . . 
-                . 2 . . . . 2 . . . . . 2 . . . 
-                . 2 . . . . 2 . . 2 . . 2 . . . 
-                . 2 . . . . 2 . . 2 2 2 2 . . . 
-                . . . . . . 2 . . . . . . . . . 
-                `, SpriteKind.Enemy)
-            Robot.y = randint(5, 150)
-            Robot.x = 325
-            enemyAmount += 1
-        }
+        spawnEnemyMain()
     } else {
         console.log("Too many enemies!")
     }
